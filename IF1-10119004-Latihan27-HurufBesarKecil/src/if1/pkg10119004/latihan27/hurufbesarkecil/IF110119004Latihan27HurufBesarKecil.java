4bbad2a6b79706d973734494913b5299565c3e2b
ramadan25 <72897383+ramadan25@users.noreply.github.com>
2020-10-30 23:33:14 +0700

    Add files via upload

---
+++ IF1-10119004-Latihan27-HurufBesarKecil/src/if1/pkg10119004/latihan27/hurufbesarkecil/IF110119004Latihan27HurufBesarKecil.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119004.latihan27.hurufbesarkecil;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ENDOG
+ * Nama     : Muhamad Ramadan
+ * Nim      : 10119004
+ * Kelas    : IF1
+ */
+public class IF110119004Latihan27HurufBesarKecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String besar, kecil;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan Kalimat : ");
+        String nama = scanner.nextLine();
+        System.out.println();
+        System.out.println("====Hasil Formatting====");
+        System.out.println("Huruf Besar : "+nama.toUpperCase());
+        System.out.println("Huruf Kecil : "+nama.toLowerCase());
+    }
+    
+}
